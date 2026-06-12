37c841a507536b70a8a2e8817141f69e930b03ec
Felype Justino <felype.justino.silva@escola.pr.gov.br>
2026-06-12 17:36:17 -0300

    .

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
 gsap.from(".hero h1",{
+duration:1.5,
 y:100,
-opacity:0,
-duration:1.5
+opacity:0
 });
 
 gsap.from(".hero p",{
-delay:.5,
-opacity:0,
-duration:1.5
+duration:2,
+opacity:0
 });
 
 let water=0;
 let energy=0;
-let trees=0;
+let co2=0;
 
 setInterval(()=>{
 
-if(water<5000) water+=15;
-if(energy<3000) energy+=10;
-if(trees<1500) trees+=5;
+if(water<10000) water+=25;
+if(energy<5000) energy+=12;
+if(co2<3000) co2+=8;
 
-document.getElementById("water").innerHTML=water;
-document.getElementById("energy").innerHTML=energy;
-document.getElementById("trees").innerHTML=trees;
+document.getElementById("water").innerText=water;
+document.getElementById("energy").innerText=energy;
+document.getElementById("co2").innerText=co2;
 
 },20);
 
-new Chart(document.getElementById('impactChart'),{
-
-type:'line',
-
+new Chart(
+document.getElementById("chart"),
+{
+type:"line",
 data:{
-labels:['Jan','Fev','Mar','Abr','Mai','Jun'],
+labels:[
+"Jan","Fev","Mar",
+"Abr","Mai","Jun"
+],
 datasets:[{
-label:'Economia de Água',
-data:[100,300,500,800,1200,1600],
-borderColor:'#52b788',
+label:"Economia de Água",
+data:[
+200,
+500,
+900,
+1500,
+2500,
+4000
+],
+borderColor:"#52b788",
+backgroundColor:
+"rgba(82,183,136,.3)",
 fill:true
 }]
-},
-
-options:{
-responsive:true
 }
-
 });
